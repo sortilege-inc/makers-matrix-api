ffeb195a51b6193d6e86b8bb8e2777a84c4a4332
Brent Ramsey <brent@hotpixelgroup.com>
2021-03-21 18:38:07 -0500

    update

--- src/libs/config/config.service.ts
+++ src/libs/config/config.service.ts
@@ -3,13 +3,13 @@ import * as fs from 'fs';
 
 export interface ConfigData {
   PORT?: string
-  // Office Database:
-  DB_DIGGSLY_TYPE: 'mysql' | 'mariadb'
-  DB_DIGGSLY_HOST?: string
-  DB_DIGGSLY_DATABASE: string
-  DB_DIGGSLY_PORT?: number
-  DB_DIGGSLY_USERNAME: string
-  DB_DIGGSLY_PASSWORD: string
+
+  DB_TYPE: 'mysql' | 'mariadb'
+  DB_HOST?: string
+  DB_DATABASE: string
+  DB_PORT?: number
+  DB_USERNAME: string
+  DB_PASSWORD: string
 }
 
 export class ConfigService {
@@ -17,7 +17,7 @@ export class ConfigService {
 
   constructor(filePath: string) {
     const envConfig: any = dotenv.parse(fs.readFileSync(filePath));
-    envConfig.DB_DIGGSLY_PORT = parseInt(envConfig.DB_DIGGSLY_PORT);
+    envConfig.DB_PORT = parseInt(envConfig.DB_PORT);
     this.vars = envConfig as ConfigData;
   }
 
